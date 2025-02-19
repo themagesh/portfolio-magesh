68004523801390f20e887fd649710a51c3f23d35
themagesh <themagesh.v@gmail.com>
2025-02-19 21:02:03 +0530

    updatae

--- portfolio/next.config.mjs
+++ portfolio/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: "export",
+    distDir: "public", // Specify the output directory
+  };
+  
+  module.exports = nextConfig;
+  
